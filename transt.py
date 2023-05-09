b845c535d0e63cb5503ba9064884daded90e7818
Raphael-08 <106021733+Raphael-08@users.noreply.github.com>
2023-05-09 21:37:56 +0530

    fixed translation

--- transt.py
+++ transt.py
@@ -1,4 +1,6 @@
-# import re
+from googletrans import Translator
+from langdetect import detect
+import re
 
 # letters = {
 #     "A": "ఏ","B": "బీ","C": "సీ","D": "డీ",
@@ -7,43 +9,62 @@
 #     "M": "ఎం","N": "ఎన్","O": "ఓ","P": "పీ",
 #     "Q": "క్యూ","R": "ఆర్","S": "ఎస్","T": "టీ",
 #     "U": "యూ","V": "వీ","W": "డబల్యూ","X": "ఎక్స్",
-#     "Y": "వై","Z": "జెడ్"
+#     "Y": "వై","Z": "జెడ్",'0':'0','1':'1','2':'2',
+#     '3':'3','4':'4','5':'5','6':'6','7':'7','8':'8',
+#     '9':'9','_':'_'
 # }
 
 # data_type = {
-#     "char": "చార్",
-#     "int": "పూర్ణం",
-#     "short": "చిన్నది",
-#     "long": "పెద్దది",
-#     "float": "ఫ్లోట్",
-#     "double": "డబల్",
-#     "void": "ఖాళీ"
-# }
-
-# sp_words = {
-#     "(ARGS)":"ఆర్గ్స్",
-#     "const" :"స్థిరంగా"
-# }
-
+#                  "char": "చార్",
+#                  "int": "పూర్ణం",
+#                  "short": "చిన్నది",
+#                  "long": "పెద్దది",
+#                  "float": "ఫ్లోట్",
+#                  "double": "డబల్",
+#                  "void": "ఖాళీ",
+#                  "unsigned":"సానుకూల సంఖ్య",
+#                  "signed":"ప్రతికూల సంఖ్య",
+#                  "struct":"నిర్మాణం",
+#                  "(args)":"(ఆర్గ్స్)",
+#                  "const" :"స్థిరంగా",
+#                  "counst":"స్థిరంగా",
+#                  "ch1":"ch1",
+#                  "":""
+#                 }
 # def variable(tv):
-#     tv = tv.replace('*', '')
-#     splt = list(tv)
-#     emptstr = ''
-#     for i in splt:
-#         emptstr += letters[i.upper()]
-#     return emptstr
+#     translator = Translator(service_urls=['translate.google.com'])
+#     tv = translator.translate(tv, dest='te').text
+#     if detect(tv)!='te':
+#         tv=tv.replace('*','')
+#         splt=list(tv)
+#         emptstr=''
+#         for i in splt:
+#                 emptstr+=letters[i.upper()]
+#         return emptstr
+#     else:
+#         return tv
 
-# def eng_to_telugu(text):
+# def eng_to_telugu(text,var,d_type,eng_txt):
+#     print(text,var,d_type)
+
 #     if text == "syntax error":
 #         return "సింటాక్స్ లోపం"
-#     pattern = re.compile(r'\b([a-zA-Z]+)\b')  
-#     telugu_text = re.sub(pattern, lambda match: data_type.get(match.group().lower(), match.group()), text)
-#     telugu_text = re.sub(pattern, lambda match: sp_words.get(match.group(), match.group()), telugu_text)
+#     if eng_txt[:13]=="bad character" :
+#         return 'సింటాక్స్ లోపం చెడు అక్షరం '+eng_txt[-3:]
+
+#     # telugu_text = re.sub(pattern, lambda match: sp_words.get(match.group(), match.group()), telugu_text)
 #     telugu_text = re.sub(pattern, lambda match: variable(match.group()), telugu_text)
+#     substr = data_type['char']
+#     regex_pat = rf'{substr}(?!.*{substr})'
+#     telugu_text = re.sub(regex_pat,data_type[d_type],text)
+#     telugu_text  = re.sub('CH1',variable(var)+'ను',text)
+#     print(telugu_text)
+#     telugu_text  = re.sub(' ను ',' ',text)
+#     print(telugu_text)
+#     telugu_text  = re.sub('CH1',variable(var)+'ను',text)
+
 #     return telugu_text
-from langdetect import detect
-from googletrans import Translator
-import re
+# import re
 
 
 letters   = {
@@ -71,13 +92,14 @@ data_type = {
                  "(args)":"(ఆర్గ్స్)",
                  "const" :"స్థిరంగా",
                  "counst":"స్థిరంగా",
+                 "chant" : "స్థిరంగా",
                  "ch1":"ch1",
                  "":""
                 }
 
 
-def eng_to_telugu(text,var,d_type,len_str_w):
-    print(text)
+def eng_to_telugu(text,var,d_type,sub_txt):
+    print(text,sub_txt)
     splited_list = text.split()
     eng_word_list =[]
 
@@ -117,14 +139,28 @@ def eng_to_telugu(text,var,d_type,len_str_w):
                 text = text.replace(eng_word_list[0],reptxt,1)
                 text = text.replace(eng_word_list[1],data_type[eng_word_list[1]],1)
                 text = text.replace(eng_word_list[2],data_type[eng_word_list[2].lower()],1)
-        text = text.replace(variable('ch1'),variable(var)+'ను',1)
+        text = text.replace(variable('ch1'),variable(var),1)
+        pattern = re.compile(r'\b([a-zA-Z]+)\b')  
+        text = re.sub(pattern, lambda match: data_type.get(match.group().lower(), match.group()), text)
         substr = data_type['char']
         regex_pat = rf'{substr}(?!.*{substr})'
         text = re.sub(regex_pat,data_type[d_type],text)
-        text = re.sub('Int',data_type['int'],text)
         text  = re.sub('CH1',variable(var),text)
-        text  = re.sub('to const char',data_type['const']+' '+data_type[d_type]+' కు',text)
-        text  = re.sub('const',data_type['const'],text)
+        text  = re.sub('Ch1',variable(var),text)
+
+        splt=text.split('(')
+        print(splt)
+        if len(splt)==2:
+            list_2 = splt[1].split(')')
+            splt[1]=list_2[0]
+            print(list_2)
+            splt.append(list_2[1])
+            text = splt[0]+'('+sub_txt+')'+splt[2]
+            print(text)
+            text = re.sub('Int',data_type['int'],text)
+
+        # text  = re.sub('to const char',data_type['const']+' '+data_type[d_type]+' కు',text)
+        # text  = re.sub('const',data_type['const'],text)
         # text = text.replace(data_type['char'],data_type[d_type],1)
         return text
     else:

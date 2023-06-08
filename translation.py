68bdad0d1ee7c3f49dd59d3ae3456d5642cc697e
Raphael-08 <106021733+Raphael-08@users.noreply.github.com>
2023-06-08 22:56:58 +0530

    organized code a little bit

--- translation.py
+++ translation.py
@@ -81,19 +81,11 @@ data_type = {
     "": "",
 }
 
-
-def transt(text):
-    print(text)
-    if text == "syntax error":
-        return data_type[text]
-    if 'bad character' in text:
-        return('సింటాక్స్ లోపం చెడు అక్షరం '+text[-3:])
+def preprocess_text(text):
     text = args_restruct(text)
     text1 = text
-
     sen_list = text.split()
     var = sen_list[0]
-
     if sen_list[-1] == 'declare':
         text = declare_restruct(text)
         if re.search(r'pointer to member of class', text):
@@ -103,153 +95,31 @@ def transt(text):
         var = cast_var(text)
         if re.search(r'pointer to member of class', text):
             text = class_restructc(text)
-
     if sen_list[-1] == "var":
         var = sen_list[-2]
-    
-    print(text)
+    return text,var
 
+def translate_text(text,var):
     telugu_text = replace_words_with_values(text, data_type)
-
     telugu_text = user_def_var(telugu_text)
-
     telugu_text = arg_var(telugu_text)
-
     telugu_text = telugu_text.replace(var, variable(var), 1)
-
     telugu_text = transt1(telugu_text)
+    return telugu_text
+
+def transt(text):
+
+    if text == "syntax error":
+        return data_type[text]
+    if 'bad character' in text:
+        return('సింటాక్స్ లోపం చెడు అక్షరం '+text[-3:])
+  
+    text,var = preprocess_text(text)
+    telugu_text = translate_text(text,var)
 
     return telugu_text
 
-def variable(text):
-    result = ''
-    if len(text) > 3:
-        text = transliterate_text(text, lang_code='te')
-        for char in text:
-            if char in letters:
-                result += letters[char]
-            else:
-                result += char
-        return result
-    else:
-        emptstr = ''
-        for i in text:
-            emptstr += letters[i.upper()]
-        return emptstr
-
-def replace_words_with_values(string, dictionary):
-    pattern = re.compile(r'\b({})\b'.format(
-        '|'.join(map(re.escape, dictionary.keys()))))
-    replaced_string = pattern.sub(lambda x: dictionary[x.group()], string)
-    return replaced_string
-
-def transt1(text):
-    text = re.sub(r'\$ 3', 'గా', text)
-    text = re.sub(r'\$ ', 'కి ', text)
-    text = re.sub(r'\$  ', 'కి ', text)
-    text = re.sub(r'కి  3', 'గా', text)
-    text = re.sub(r'కి  శ్రేణి', ' యొక్క శ్రేణి', text)
-    text = re.sub(r'కి శ్రేణి', ' యొక్క శ్రేణి', text)
-    telugu_text = replace_words_with_values(text, data_type)
-    return telugu_text
-
-def user_def_var(text):
-    matches = re.findall(r'\bనిర్మాణం\s+(\w+)',text)
-    if matches:
-        for i in matches:
-            text = text.replace(i,variable(i))
-    matches1 = re.findall(r'\bయూనియన్\s+(\w+)',text)
-    if matches1:
-        for i in matches1:
-            text = text.replace(i,variable(i))
-    matches2 = re.findall(r'\bఇనమ్‌\s+(\w+)',text)
-    if matches2:
-        for i in matches2:
-            text = text.replace(i,variable(i))
-    matches3 = re.findall(r'\bక్లాస్‌\s+(\w+)',text)
-    if matches3:
-        for i in matches3:
-            text = text.replace(i,variable(i))
-    return text
-
-def typec_eng(text):
-    pattern = r'\((.*?)\)'
-    matches = re.findall(pattern, text)
-    return matches[0].split(',')
-
-def cast_restruct(string):
-    pattern = r'block\s*(?:\([^)]*\))?\s*returning'
-    pattern1 = r'function\s*(?:\([^)]*\))?\s*returning'
-    match = re.search(pattern, string)
-    match1 = re.search(pattern1, string)
-    if match:
-        extracted_string = match.group(0)
-        updated_string = re.sub(pattern, '', string)
-        return extracted_string+" "+updated_string
-    elif match1:
-        extracted_string = match1.group(0)
-        updated_string = re.sub(pattern1, '', string)
-        return extracted_string+" "+updated_string
-    else:
-        return string
-
-def declare_restruct(string):
-    pattern = r'function\s*(?:\([^)]*\))?\s*returning'
-    match = re.search(pattern, string)
-    if match:
-        extracted_string = match.group(0)
-        updated_string = re.sub(pattern, '', string)
-        updated_string = updated_string.split('^')
-        return updated_string[0]+extracted_string+" "+updated_string[1]
-    else:
-        a = string.split('^')
-        return a[0]+' '+a[1]
-
-def cast_var(string):
-    pattern = r'7\s+(\w+)\s+0'
-    match = re.search(pattern, string)
-    if match:
-        extracted_word = match.group(1)
-        print(extracted_word)
-        return extracted_word
-    
-def class_restructd(text1):
-    match = re.search(r'\^(.*?)\$',text1)
-    data = match.group(1)+'$'
-    matches = re.findall(r'\bclass\s+(\w+)', text1)
-    text1 = text1.replace('^'+data,'^')
-    match = re.search(r'function\s*(?:\([^)]*\))?\s*returning', text1)
-    if match:
-        extracted_string = match.group(0)
-        updated_string = re.sub(r'function\s*(?:\([^)]*\))?\s*returning', '##', text1)
-        text1 = updated_string.replace('##',extracted_string+' '+data)
-        text1= text1.replace('pointer to member of class '+matches[-1],'class '+matches[-1]+' member of pointer to',1)
-        return text1.replace('^ ','')
-    else:
-        sp = text1.split(matches[-1])
-        text1= sp[0]+matches[-1]+' '+data+sp[-1]
-        text1= text1.replace('pointer to member of class '+matches[-1],'class '+matches[-1]+' member of pointer to',1)
-        return text1.replace('^ ','')
-
-def class_restructc(text):
-    matches = re.findall(r'\bclass\s+(\w+)', text)
-    text= text.replace('pointer to member of class '+matches[0],'class '+matches[0]+' member of pointer to',1)
-    return text
-
-def arg_var(telugu_text):
-    if '(' in telugu_text:
-        type_w = typec_eng(telugu_text)
-        for i in range(len(type_w)):
-            word = type_w[i].strip()
-            if re.match(r'^[a-zA-Z0-9]+$', word):
-                r = variable(word)
-                type_w[i] = r
-        a = telugu_text.split('(')
-        b = a[1].split(')')
-        telugu_text = a[0]+'('+','.join(type_w)+')'+b[1]
-        return telugu_text
-    else:
-        return telugu_text
+#----------------------preprocess functions
 
 def args_restruct(text):
     list1 = ''
@@ -292,3 +162,134 @@ def args_restruct(text):
             return list1[0]+'('+','.join(splt)+')'+list2[1]
     else:
         return text
+
+def declare_restruct(string):
+    pattern = r'function\s*(?:\([^)]*\))?\s*returning'
+    match = re.search(pattern, string)
+    if match:
+        extracted_string = match.group(0)
+        updated_string = re.sub(pattern, '', string)
+        updated_string = updated_string.split('^')
+        return updated_string[0]+extracted_string+" "+updated_string[1]
+    else:
+        a = string.split('^')
+        return a[0]+' '+a[1]
+
+def cast_restruct(string):
+    pattern = r'block\s*(?:\([^)]*\))?\s*returning'
+    pattern1 = r'function\s*(?:\([^)]*\))?\s*returning'
+    match = re.search(pattern, string)
+    match1 = re.search(pattern1, string)
+    if match:
+        extracted_string = match.group(0)
+        updated_string = re.sub(pattern, '', string)
+        return extracted_string+" "+updated_string
+    elif match1:
+        extracted_string = match1.group(0)
+        updated_string = re.sub(pattern1, '', string)
+        return extracted_string+" "+updated_string
+    else:
+        return string
+
+def cast_var(string):
+    pattern = r'7\s+(\w+)\s+0'
+    match = re.search(pattern, string)
+    if match:
+        extracted_word = match.group(1)
+        return extracted_word
+    
+def class_restructd(text1):
+    match = re.search(r'\^(.*?)\$',text1)
+    data = match.group(1)+'$'
+    matches = re.findall(r'\bclass\s+(\w+)', text1)
+    text1 = text1.replace('^'+data,'^')
+    match = re.search(r'function\s*(?:\([^)]*\))?\s*returning', text1)
+    if match:
+        extracted_string = match.group(0)
+        updated_string = re.sub(r'function\s*(?:\([^)]*\))?\s*returning', '##', text1)
+        text1 = updated_string.replace('##',extracted_string+' '+data)
+        text1= text1.replace('pointer to member of class '+matches[-1],'class '+matches[-1]+' member of pointer to',1)
+        return text1.replace('^ ','')
+    else:
+        sp = text1.split(matches[-1])
+        text1= sp[0]+matches[-1]+' '+data+sp[-1]
+        text1= text1.replace('pointer to member of class '+matches[-1],'class '+matches[-1]+' member of pointer to',1)
+        return text1.replace('^ ','')
+
+def class_restructc(text):
+    matches = re.findall(r'\bclass\s+(\w+)', text)
+    text= text.replace('pointer to member of class '+matches[0],'class '+matches[0]+' member of pointer to',1)
+    return text
+
+#----------------------translate functions
+
+def replace_words_with_values(string, dictionary):
+    pattern = re.compile(r'\b({})\b'.format(
+        '|'.join(map(re.escape, dictionary.keys()))))
+    replaced_string = pattern.sub(lambda x: dictionary[x.group()], string)
+    return replaced_string
+
+def user_def_var(text):
+    matches = re.findall(r'\bనిర్మాణం\s+(\w+)',text)
+    if matches:
+        for i in matches:
+            text = text.replace(i,variable(i))
+    matches1 = re.findall(r'\bయూనియన్\s+(\w+)',text)
+    if matches1:
+        for i in matches1:
+            text = text.replace(i,variable(i))
+    matches2 = re.findall(r'\bఇనమ్‌\s+(\w+)',text)
+    if matches2:
+        for i in matches2:
+            text = text.replace(i,variable(i))
+    matches3 = re.findall(r'\bక్లాస్‌\s+(\w+)',text)
+    if matches3:
+        for i in matches3:
+            text = text.replace(i,variable(i))
+    return text
+
+def typec_eng(text):
+    pattern = r'\((.*?)\)'
+    matches = re.findall(pattern, text)
+    return matches[0].split(',')
+
+def arg_var(telugu_text):
+    if '(' in telugu_text:
+        type_w = typec_eng(telugu_text)
+        for i in range(len(type_w)):
+            word = type_w[i].strip()
+            if re.match(r'^[a-zA-Z0-9]+$', word):
+                r = variable(word)
+                type_w[i] = r
+        a = telugu_text.split('(')
+        b = a[1].split(')')
+        telugu_text = a[0]+'('+','.join(type_w)+')'+b[1]
+        return telugu_text
+    else:
+        return telugu_text
+
+def variable(text):
+    result = ''
+    if len(text) > 3:
+        text = transliterate_text(text, lang_code='te')
+        for char in text:
+            if char in letters:
+                result += letters[char]
+            else:
+                result += char
+        return result
+    else:
+        emptstr = ''
+        for i in text:
+            emptstr += letters[i.upper()]
+        return emptstr
+
+def transt1(text):
+    text = re.sub(r'\$ 3', 'గా', text)
+    text = re.sub(r'\$ ', 'కి ', text)
+    text = re.sub(r'\$  ', 'కి ', text)
+    text = re.sub(r'కి  3', 'గా', text)
+    text = re.sub(r'కి  శ్రేణి', ' యొక్క శ్రేణి', text)
+    text = re.sub(r'కి శ్రేణి', ' యొక్క శ్రేణి', text)
+    telugu_text = replace_words_with_values(text, data_type)
+    return telugu_text

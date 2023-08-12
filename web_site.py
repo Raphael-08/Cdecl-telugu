f816ac2aec35692b476086b6a27b96343c5c453b
Mohan Ranganath Varma Vummadisetty <106021733+Raphael-08@users.noreply.github.com>
2023-08-12 22:25:13 +0530

    Update and rename translator.py to web_site.py

--- web_site.py
+++ web_site.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 from functools import lru_cache
 from flask import Flask, request, render_template, jsonify
-from translation import transt
+from translation.code_trans import to_Tel
 
 app = Flask(__name__)
 
@@ -11,45 +11,44 @@ app = Flask(__name__)
 def index():
     print(f"translate : {translate.cache_info()}")
     if request.method == "POST":
-        query = request.form.get("query")
+        query: str | None = request.form.get("query")
         if query:
             if query == "help":
                 return jsonify({"output": "సింటాక్స్ లోపం"})
-            return jsonify({"output": translate(query)})
+            return jsonify({"output": translate(query.strip())})
         return jsonify({"error": "Error"})
     return render_template("index.html")
 
 
 SYNTAX_ERROR = "syntax error"
 
-current_directory = os.getcwd()
-command = [os.path.join(current_directory, "cdecl")]
+current_directory: str = os.getcwd()
+command: list[str] = [os.path.join(current_directory, "cdecl")]
 
 
 @lru_cache(None)
-def translate(query):
-    storage_classes = ["auto", "extern", "static", "register"]
-    q_l = query.split()
+def translate(query: str) -> str:
+    storage_classes: list[str] = ["auto", "extern", "static", "register"]
+    q_l: list[str] = query.split()
     if q_l[0] in ("declare", "cast"):
-        return transt(SYNTAX_ERROR)
+        return to_Tel(SYNTAX_ERROR)
     if len(q_l) < 3 and q_l[0] in storage_classes:
         query = f"{q_l[0]} int {q_l[1]}"
-    queries = [query, f"explain {query};", f"declare {query};"]
+    queries: list[str] = [query, f"explain {query};", f"declare {query};"]
 
     translated_text = None
     with subprocess.Popen(
         command, stdin=subprocess.PIPE, stdout=subprocess.PIPE
     ) as process:
         output, _ = process.communicate(input="\n".join(queries).encode())
-        print(output, _)
         for line in output.splitlines():
             line = line.decode()
             if line and line != SYNTAX_ERROR:
                 print(line)
-                translated_text = transt(line)
+                translated_text = to_Tel(line)
                 break
 
-    return translated_text or transt(SYNTAX_ERROR)
+    return translated_text or to_Tel(SYNTAX_ERROR)
 
 
 if __name__ == "__main__":
